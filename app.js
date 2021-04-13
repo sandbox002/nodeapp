cb2e3a210d171886a037ca8fc26a61c9963aec8c
sandbox002 <82431774+sandbox002@users.noreply.github.com>
2021-04-13 19:48:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgn');
 
 var flash = require('express-flash');
 var session = require('express-session');
