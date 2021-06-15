11ebdc1f9192b2e6d52e0c179c943205b1095ab0
sangbum kim <sb103601@sk.com>
2021-06-15 12:24:36 +0900

    first

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SandBox CI/CD Pipeline' });
+  res.render('index', { title: 'Cloud Architect W/S' });
 });
 
 module.exports = router;
